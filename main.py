33c74cf3f716d15c1bc5b204b14c3b30a06f66e1
barchitect0 <tbng84@gmail.com>
2024-10-14 17:02:28 +0700

    load env

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
+import os
 from web3 import Web3
 from eth_account import Account
 import json
+import logging
 
 def generate_ethereum_wallets(num_wallets):
-  wallets = []
-  for _ in range(num_wallets):
-    account = Account.create()
-    wallets.append({
-      'address': account.address,
-      'private_key': account.key.hex()
-    })
-  return wallets
+    wallets = []
+    for _ in range(num_wallets):
+        account = Account.create()
+        wallets.append({
+        'address': account.address,
+        'private_key': account.key.hex()
+        })
+    return wallets
 
 def save_wallets_to_file(wallets, filename='wallets.json'):
-  with open(filename, 'w') as file:
-    json.dump(wallets, file, indent=4)
+    with open(filename, 'w') as file:
+        json.dump(wallets, file, indent=4)
+
+def generate_wallets():
+    num_wallets = int(input("Enter the number of Ethereum wallets to generate: "))
+    wallets = generate_ethereum_wallets(num_wallets)
+    save_wallets_to_file(wallets)
+    print(f"{num_wallets} Ethereum wallets have been generated and saved to wallets.json")
 
 if __name__ == "__main__":
-  num_wallets = int(input("Enter the number of Ethereum wallets to generate: "))
-  wallets = generate_ethereum_wallets(num_wallets)
-  save_wallets_to_file(wallets)
-  print(f"{num_wallets} Ethereum wallets have been generated and saved to wallets.json")
+    from dotenv import load_dotenv
+    load_dotenv()
+    logging.basicConfig(level=logging.INFO)
+
+    w3 = Web3(Web3.HTTPProvider(os.environ.get('RPC_ENDPOINT')))
+    operator = w3.eth.account.from_key(os.environ.get('PRIVATE_KEY'))
+
+    print(f"Operator address: {operator.address}")
+
+    #generate_wallets()
