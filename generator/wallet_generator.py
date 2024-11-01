2870a141df6e41bc3e159f917ce9c400c9e843a7
barchitect0 <tbng84@gmail.com>
2024-11-01 10:29:49 +0700

    minor fix

--- generator/wallet_generator.py
+++ generator/wallet_generator.py
@@ -20,8 +20,8 @@ class WalletGenerator(metaclass=Singleton):
         with open(filename, 'w') as file:
             json.dump(wallets, file, indent=4)
 
-#     def generate_wallets(self):
-#         num_wallets = int(input("Enter the number of Ethereum wallets to generate: "))
-#         wallets = self.generate_ethereum_wallets(num_wallets)
-#         self.save_wallets_to_file(wallets)
-#         print(f"{num_wallets} Ethereum wallets have been generated and saved to wallets.json")
+    def generate_wallets(self):
+        num_wallets = int(input("Enter the number of Ethereum wallets to generate: "))
+        wallets = self.generate_ethereum_wallets(num_wallets)
+        self.save_wallets_to_file(wallets)
+        print(f"{num_wallets} Ethereum wallets have been generated and saved to wallets.json")
