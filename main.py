50feb1281c4a71cbd265e0082ed200f32250d14b
barchitect0 <tbng84@gmail.com>
2024-11-04 10:52:10 +0700

    increase batch size

--- main.py
+++ main.py
@@ -28,5 +28,5 @@ if __name__ == "__main__":
     # native transfer
     with open('wallets.json') as file:
         wallets = json.load(file)
-        executor = NativeTransferExecutor(os.environ.get('RPC_ENDPOINT'), os.environ.get('PRIVATE_KEY'), wallets[:200], 100000)
+        executor = NativeTransferExecutor(os.environ.get('RPC_ENDPOINT'), os.environ.get('PRIVATE_KEY'), wallets[:500], 100000)
         executor.execute({'amount': 0.01})
