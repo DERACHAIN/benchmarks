01ff33aeb553d7fa32821dec8850995fa3a3dbe5
barchitect0 <tbng84@gmail.com>
2024-11-04 17:28:19 +0700

    command arguments

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 import os
-from web3 import Web3
-from eth_account import Account
-#from web3.middleware.signing import construct_sign_and_send_raw_middleware
 import json
 import logging
+import argparse
 
 from generator import WalletGenerator
 from executor import NativeTransferExecutor, Bootstrapper
+
 
 if __name__ == "__main__":
     from dotenv import load_dotenv
     load_dotenv()
     logging.basicConfig(filename='logs.txt', level=logging.INFO, 
                         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    
+    parser = argparse.ArgumentParser(description='Benchmark bot.')    
+    parser.add_argument('-a', '--action', type=str, help="Actions: generate_wallets | bootstrap | native_transfer")
 
-    # create wallets
-    #generate_wallets()
-    #wg = WalletGenerator()
-    #wg.generate_wallets()
+    args = parser.parse_args()
 
-    # bootstrap
-    bootstrapper = Bootstrapper(os.environ.get('RPC_ENDPOINT'), os.environ.get('PRIVATE_KEY'), 1)
-    # with open('wallets.json') as file:
-    #     wallets = json.load(file)
-    #     for wallet in wallets:
-    #         bootstrapper.execute({'to': wallet['address']})
-
-    # native transfer
-    with open('wallets.json') as file:
-        wallets = json.load(file)
-        executor = NativeTransferExecutor(os.environ.get('RPC_ENDPOINT'), os.environ.get('PRIVATE_KEY'), wallets[:1000], 10**6)
-        executor.execute({'amount': 0.01})
+    if args.action == 'generate_wallets':
+        wg = WalletGenerator()
+        wg.generate_wallets()
+    elif args.action == 'bootstrap':
+        bootstrapper = Bootstrapper(os.environ.get('RPC_ENDPOINT'), os.environ.get('PRIVATE_KEY'), 10)
+        with open('wallets.json') as file:
+            wallets = json.load(file)
+            for wallet in wallets:
+                bootstrapper.execute({'to': wallet['address']})
+    elif args.action == 'native_transfer':        
+        with open('wallets.json') as file:
+            wallets = json.load(file)
+            executor = NativeTransferExecutor(os.environ.get('RPC_ENDPOINT'), os.environ.get('PRIVATE_KEY'), wallets[:1000], 10**6)
+            executor.execute({'amount': 0.01})
