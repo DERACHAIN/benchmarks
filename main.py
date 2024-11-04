8b47ae9dbdbfcd25ce773480adca9c7c49219f4e
barchitect0 <tbng84@gmail.com>
2024-11-04 18:28:42 +0700

    dynamic arguments

--- main.py
+++ main.py
@@ -15,14 +15,17 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description='Benchmark bot.')    
     parser.add_argument('-a', '--action', type=str, help="Actions: generate_wallets | bootstrap | native_transfer")
+    parser.add_argument('-b', '--balance', type=float, help="Initial balance of wallets", default=10)
+    parser.add_argument('-n', '--number', type=int, help="Number of wallets", default=100)
+    parser.add_argument('-t', '--tx', type=int, help="Number of tx", default=10**6)
 
     args = parser.parse_args()
 
     if args.action == 'generate_wallets':
         wg = WalletGenerator()
         wg.generate_wallets()
-    elif args.action == 'bootstrap':
-        bootstrapper = Bootstrapper(os.environ.get('RPC_ENDPOINT'), os.environ.get('PRIVATE_KEY'), 10)
+    elif args.action == 'bootstrap':        
+        bootstrapper = Bootstrapper(os.environ.get('RPC_ENDPOINT'), os.environ.get('PRIVATE_KEY'), args.balance)
         with open('wallets.json') as file:
             wallets = json.load(file)
             for wallet in wallets:
@@ -30,7 +33,7 @@ if __name__ == "__main__":
     elif args.action == 'native_transfer':        
         with open('wallets.json') as file:
             wallets = json.load(file)
-            executor = NativeTransferExecutor(os.environ.get('RPC_ENDPOINT'), os.environ.get('PRIVATE_KEY'), wallets[:1000], 10**6)
+            executor = NativeTransferExecutor(os.environ.get('RPC_ENDPOINT'), os.environ.get('PRIVATE_KEY'), wallets[:args.number], args.tx)
             executor.execute({'amount': 0.01})
     
     logging.info(f"Action {args.action} completed")
