384dbd0e310fafd2e9583fb102c4a2e79ffbae7f
barchitect0 <tbng84@gmail.com>
2024-11-06 17:12:12 +0700

    refactor

--- executor/native_transfer_executor.py
+++ executor/native_transfer_executor.py
@@ -14,7 +14,7 @@ class NativeTransferExecutor(BaseExecutor):
         self.wallets = [self.create_wallet(wallet) for wallet in wallets]
         self.total_tx = total_tx
 
-#     def create_wallet(self, wallet):
+    def create_wallet(self, wallet):
 #         return self.w3.eth.account.from_key(wallet['private_key'])
 
 #     def execute(self, data):
