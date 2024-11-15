470220c7dc39690bff1fbbca4b999d76e62b1930
barchitect0 <tbng84@gmail.com>
2024-11-15 14:33:34 +0700

    minor fix

--- executor/transfer_executor.py
+++ executor/transfer_executor.py
@@ -9,7 +9,7 @@ import random
 
 class TransferExecutor(BaseExecutor):
     def __init__(self, rpc, operator_sk, erc20_address, erc721_address, erc20_abi, erc721_abi, wallets, total_tx=10**5):
-#         super().__init__(rpc, operator_sk)
+        super().__init__(rpc, operator_sk)
 
 #         self.w3 = Web3(Web3.HTTPProvider(rpc))
 #         self.w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
